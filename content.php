3a6d3c0fe8ec040696fa75d19437750b699461a1
christiangranadosmonroy <christiangranadosmonroy@outlook.com>
2026-03-20 22:42:17 -0600

    "fix: seguridad en content.php
    - fix: redirect sesion expirada via SESSION en lugar de GET
    - fix: module con valor default para evitar warning de PHP
    - refactor: $_GET['module'] a variable \$module"

--- content.php
+++ content.php
@@ -3,61 +3,48 @@ require_once "config/database.php";
 require_once "config/fungsi_tanggal.php";
 require_once "config/fungsi_rupiah.php";
 
+if (empty($_SESSION['username']) && empty($_SESSION['password'])) {
+    $_SESSION['alert'] = 1;
+    header("Location: index.php");
+    exit;
+} else {
+    $module = $_GET['module'] ?? 'start';
 
-if (empty($_SESSION['username']) && empty($_SESSION['password'])){
-	echo "<meta http-equiv='refresh' content='0; url=index.php?alert=1'>";
-}
-else {
-	if ($_GET['module'] == 'start') {
-		include "modules/start/view.php";
-	}
+    if ($module == 'start') {
+        include "modules/start/view.php";
 
-	elseif ($_GET['module'] == 'productos') {
-		include "modules/productos/view.php";
-	}
+    } elseif ($module == 'productos') {
+        include "modules/productos/view.php";
 
-	elseif ($_GET['module'] == 'form_productos') {
-		include "modules/productos/form.php";
-	}
-	
+    } elseif ($module == 'form_productos') {
+        include "modules/productos/form.php";
 
-	elseif ($_GET['module'] == 'productos_transaction') {
-		include "modules/productos_transaction/view.php";
-	}
+    } elseif ($module == 'productos_transaction') {
+        include "modules/productos_transaction/view.php";
 
-	elseif ($_GET['module'] == 'form_productos_transaction') {
-		include "modules/productos_transaction/form.php";
-	}
-	
+    } elseif ($module == 'form_productos_transaction') {
+        include "modules/productos_transaction/form.php";
 
-	elseif ($_GET['module'] == 'stock_inventory') {
-		include "modules/stock_inventory/view.php";
-	}
+    } elseif ($module == 'stock_inventory') {
+        include "modules/stock_inventory/view.php";
 
-	elseif ($_GET['module'] == 'stock_report') {
-		include "modules/stock_report/view.php";
-	}
+    } elseif ($module == 'stock_report') {
+        include "modules/stock_report/view.php";
 
-	elseif ($_GET['module'] == 'user') {
-		include "modules/user/view.php";
-	}
+    } elseif ($module == 'user') {
+        include "modules/user/view.php";
 
+    } elseif ($module == 'form_user') {
+        include "modules/user/form.php";
 
-	elseif ($_GET['module'] == 'form_user') {
-		include "modules/user/form.php";
-	}
+    } elseif ($module == 'profile') {
+        include "modules/profile/view.php";
 
-	elseif ($_GET['module'] == 'profile') {
-		include "modules/profile/view.php";
-		}
+    } elseif ($module == 'form_profile') {
+        include "modules/profile/form.php";
 
-
-	elseif ($_GET['module'] == 'form_profile') {
-		include "modules/profile/form.php";
-	}
-
-	elseif ($_GET['module'] == 'password') {
-		include "modules/password/view.php";
-	}
+    } elseif ($module == 'password') {
+        include "modules/password/view.php";
+    }
 }
 ?>
